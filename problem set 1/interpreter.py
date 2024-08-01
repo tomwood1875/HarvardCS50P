80a3149eb98cb2ddaf9aea0d612630f1019b9beb
tom wood <37484520+tomwood1875@users.noreply.github.com>
2024-08-01 13:30:05 +0100

    progress

--- problem set 1/interpreter.py
+++ problem set 1/interpreter.py
@@ -1,10 +1,19 @@
-x = float(input("Expression: ")) 
-#y = + or - or * or /
-z = float(input)
+expression = input("Expression: ")
 
+x, y, z = expression.split()
 
+x = float(x)
+z = float(z)
 
+if y == '+':
+    result = x+z
+elif y == '-':
+    result = x-z
+elif y == '*':
+    result = x*z
+elif y == '/':
+    result = x/z
+else:
+    print("Error: Invalid Operator Selected")
 
-# if(x.contains("*"):
-#    x ** z
-#    print(expression)
+print(result)
